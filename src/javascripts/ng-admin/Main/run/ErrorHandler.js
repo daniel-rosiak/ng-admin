a2358038498521585adacf867e18a848641a682f
Daniel Rosiak <danielrosiak@gmail.com>
2016-02-08 15:55:06 +0100

    Disable build errorHadler

--- src/javascripts/ng-admin/Main/run/ErrorHandler.js
+++ src/javascripts/ng-admin/Main/run/ErrorHandler.js
@@ -1,12 +1,6 @@
 export default function errorHandler($rootScope, $state, notification) {
     $rootScope.$on("$stateChangeError", function handleError(event, toState, toParams, fromState, fromParams, error) {
-        if (error.status == 404) {
-            $state.go('ma-404');
-            event.preventDefault();
-        } else {
-            notification.log('State change error: ' + error.message, { addnCls: 'humane-flatty-error' });
-            throw error;
-        }
+        
     });
 }
 
